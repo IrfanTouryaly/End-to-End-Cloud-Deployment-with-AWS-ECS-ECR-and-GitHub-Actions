73214b4ba967b7fe2d5170d8f6fb7ed810899447
Syed Muhammad Irfan Touryaly <syedirfantouryalay1234567@gmail.com>
2025-08-20 18:45:28 +0500

    3rd commit

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "App is running! and took 3 hrs"
+    return "App is running! and took 3 hrs hehehheheheh"
 
 @app.route("/health")
 def health():
-    return "healthy is not i am", 200   # Simple text response
+    return "the container is healthy", 200   # Simple text response
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
