de78b032b1ff116bcdd19a681b50273e943d63ac
Syed Muhammad Irfan Touryaly <syedirfantouryalay1234567@gmail.com>
2025-08-21 19:13:17 +0500

    new commit chnaing code in app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "App is running! and took 3 hrs hehehhehehehc umair"
+    return "Hello from ECS FARGATE with ALB using Github Actions"
 
 @app.route("/health")
 def health():
